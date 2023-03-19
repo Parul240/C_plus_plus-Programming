b1b8b13f2d88f3912d1e330f102105ede5cdffc6
Parul Sharma <114149620+Parul240@users.noreply.github.com>
2023-03-19 17:48:08 +0530

    Create addition_of_variable.cpp

---
+++ PROBLEMS/addition_of_variable.cpp
@@ -0,0 +1,19 @@
+Write a program which does the following
+
+* Declare two variables a and b
+* Assign the value 23 to a and 20 to b
+* Output the sum of a and b to the console
+
+
+
+SOLUTION:
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int a = 23 ;      //Assign the value 23 to a
+  int b = 20 ;      //Assign the value 20 to b
+  cout << a + b ;   //cout the desired output
+  return 0;
+}
