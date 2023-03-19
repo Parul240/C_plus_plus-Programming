799b3920ca0b066eb1a06d8b2668011f8f626108
Parul Sharma <114149620+Parul240@users.noreply.github.com>
2023-03-19 17:44:06 +0530

    Create area_of_rectangle.cpp

---
+++ PROBLEMS/area_of_rectangle.cpp
@@ -0,0 +1,19 @@
+/*
+Write a program which does the following
+
+Find out and display the area of a rectangle of sides 45 and 76 respectively.
+Declare variables length, width and area and assign the relevant values to them
+Make sure to add some text as well, so that the person reading the output knows what the output means.
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int length = 45 ;
+  int width = 76 ;
+  int area = length * width ;
+  cout << "The Area of the given rectangle is: " ;
+  cout << area ; 
+  return 0;
+}
