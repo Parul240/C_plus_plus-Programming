9697009f84daca281a3fbe2427bf6c58a7f7cf8a
Parul Sharma <114149620+Parul240@users.noreply.github.com>
2023-03-22 18:13:23 +0530

    Update Strings.cpp

--- Strings.cpp
+++ Strings.cpp
@@ -7,10 +7,12 @@ Before declaring a variable as string the string datatype must be used
 
 TASK:
 
+/*
 Write a program which does the following
 
 1. Create a string named fruit and assign the value "Apple" to it.
 2. Output fruit to the console.
+*/
  
 SOLUTION:
 
