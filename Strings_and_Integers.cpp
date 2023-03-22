ff5d0533d45c95739e1262861b875824e8ccf2ed
Parul Sharma <114149620+Parul240@users.noreply.github.com>
2023-03-22 18:06:02 +0530

    Create Strings_and_Integers.cpp

---
+++ Strings_and_Integers.cpp
@@ -0,0 +1,31 @@
+C++ uses the + sign for both addition and concatenation.
+
+- Numbers are added.
+- Strings are concatenated.
+- We cannot mix the two
+
+
+TASK:
+
+You are given a program which does the following
+
+- You want to output 2569
+- Try running the given code given in the IDE as it is - it will give a Compilation error
+- Fix the error so that both the variables become strings and the output is 2569
+  
+  
+  
+SOLUTION:
+
+//We need to output 2569 using string concatenation.
+//The code below is incorrect - Debug the code to solve the problem
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main () {
+  string num1 = "25" ;
+  int num2 = 69;
+  cout << num1 + num2;
+  return 0;
+}
