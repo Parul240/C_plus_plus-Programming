59e6173e7521db792c4c8a038ab96d7323a7d2c0
Parul Sharma <114149620+Parul240@users.noreply.github.com>
2023-03-26 17:04:31 +0530

    Update Arrays.cpp

--- Arrays.cpp
+++ Arrays.cpp
@@ -1,3 +1,6 @@
+                                                          1. CREATING ARRAYS
+
+
 Arrays are used to store multiple values in a single variable, instead of declaring separate variables for each value.
 
 To declare an array,
@@ -31,3 +34,76 @@ int main() {
   cout << "Done";
   return 0;
 }
+
+
+
+                                       2. INDEX & PRINTING SPECIFIC ELEMENTS OF AN ARRAY
+ 
+ 
+You access an array element by referring to the index number inside square brackets [].
+
+                                         
+                                                              0    1     2    - index numbers
+     for eg - num[ 3 ] = {1 , 100 , 200};
+             cout < num[ 1 ]
+             output = 100  
+                              
+Note - Indexing always starts from 0 starting from left to right.
+
+               
+PROBLEM:
+
+/*               
+Write a program which does the following
+
+- Output the 3rd element from the given array to the console.
+*/
+               
+               
+               
+SOLUTION:
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  int num[ 5 ] = {1, 2, 3, 4, 5 };
+  cout << num[ 2 ];
+  return 0;
+}
+                                       
+                                       
+                                       
+                                       
+                                       3. CHANGING THE ELEMENTS OF AN ARRAY
+                                       
+
+To change the value of a specific element, we can refer to the index number.
+                                         
+
+PROBLEM:
+
+/*
+Write a program which does the following
+
+- The 3rd month in the given list is incorrect
+- Update the 3rd month in the given array with the correct one - "Mar"
+- Once the 3rd array element is updated, output the 3rd array element to the console
+*/
+                                         
+
+SOLUTION:
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string mnts[4] = {"Jan", "Feb", " Dec", "Apr"};
+  mnts[ 3 ] = "Mar";
+  cout << mnts[3];
+  return 0;
+}
+                                       
+                                       
