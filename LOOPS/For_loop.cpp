fdff0f7c64c597bc7994587dd0249cb50f3bcd02
Parul Sharma <114149620+Parul240@users.noreply.github.com>
2023-03-28 17:46:38 +0530

    Create For_loop.cpp

---
+++ LOOPS/For_loop.cpp
@@ -0,0 +1,42 @@
+* The for loop is a control structure in C++ that allows you to execute a block of code repeatedly for a specific number of times or until a certain condition is met. 
+
+* The syntax of a for loop is as follows:
+                      
+             +--------------------------------------------------------------+
+             |     for (initialization; condition; increment/decrement) {   |
+             |       // block of code to be executed                        |
+             |     }                                                        |
+             +--------------------------------------------------------------+
+               
+     Here,
+
+         * initialization - initializes variables and is executed only once
+         * condition - if true, the body of for loop is executed
+           if false, the for loop is terminated
+         * The increment/decrement - is executed at the end of each iteration and is used to modify the loop variable.
+             
+             
+* Here is an example of a for loop that prints the numbers 0 to 9:  
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+        for (int i = 0; i < 10; i++) {
+          std::cout << i << std::endl;
+}
+    return 0;
+}
+
+OUTPUT:
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
