c6a0059b09ed90df3294695f6e7ef87377476916
Parul Sharma <114149620+Parul240@users.noreply.github.com>
2023-03-28 16:49:40 +0530

    Update DoWhile_loop.cpp

--- LOOPS/DoWhile_loop.cpp
+++ LOOPS/DoWhile_loop.cpp
@@ -1,2 +1,23 @@
-In C++, a do-while loop is a control flow statement that executes a block of code at least once, and then repeats the execution of the block of code as long as the specified condition is true. 
-The basic syntax of a do-while loop is as follows:
+                                                                DO WHILE LOOP
+
+
+
+
+* In C++, a do-while loop is a control flow statement that executes a block of code at least once, and then repeats the execution of the block of code as long as the specified condition is true. 
+
+* The basic syntax of a do-while loop is as follows:
+
+                                  +------------------------------------+
+                                  |      do {                          |
+                                  |         // code to be executed     |
+                                  |     } while(condition);            |
+                                  |                                    |
+                                  +----------------------------------- + 
+
+                                    
+* The do-while loop consists of the following parts:
+
+    - The keyword do which indicates the start of the loop.
+    - The block of code to be executed, enclosed in curly braces { }.
+    - The keyword while which indicates the end of the loop.
+    - The condition to be checked for each iteration of the loop, which is specified inside the parentheses ( ) following the keyword while.                                    
