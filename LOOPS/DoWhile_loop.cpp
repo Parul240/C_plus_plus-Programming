f7b89860da67414bfbeebef5acaf28b190b2483d
Parul Sharma <114149620+Parul240@users.noreply.github.com>
2023-03-28 17:08:00 +0530

    Update DoWhile_loop.cpp

--- LOOPS/DoWhile_loop.cpp
+++ LOOPS/DoWhile_loop.cpp
@@ -22,9 +22,58 @@
     - The keyword while which indicates the end of the loop.
     - The condition to be checked for each iteration of the loop, which is specified inside the parentheses ( ) following the keyword while.                                    
 
+
       
-      
-      
+* How does a do-While loop execute?
+  
+    1. Control falls into the do-while loop.
+    2. The statements inside the body of the loop get executed.
+    3. Updation takes place.
+    4. The flow jumps to Condition
+    5. Condition is tested. 
+        - If the Condition yields true, go to Step 6.
+        - If the Condition yields false, the flow goes outside the loop
+    6. The flow goes back to Step 2.
+    7. The do-while loop has been ended and flow has gone outside the loop.  
      
+     
+     
+PROGRAM:
+
+/*
+print the value of a
+*/
+
+
+SOLUTION:
+
+#include <iostream>
+using namespace std;
+ 
+int main () {
+   // Local variable declaration:
+   int a = 10;
+
+   // do loop execution
+   do {
+      cout << "value of a: " << a << endl;
+      a = a + 1;
+   } while( a < 20 );
+ 
+   return 0;
+}
+  
+
+OUTPUT:
+value of a: 10
+value of a: 11
+value of a: 12
+value of a: 13
+value of a: 14
+value of a: 15
+value of a: 16
+value of a: 17
+value of a: 18
+value of a: 19
        
      
