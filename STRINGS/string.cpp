c2e20ce147d75f305837150ddda381464cc112d6
Parul Sharma <114149620+Parul240@users.noreply.github.com>
2023-03-29 15:07:15 +0530

    Create string.cpp

---
+++ STRINGS/string.cpp
@@ -0,0 +1,33 @@
+                                                                STRINGS
+                                               
+
+
+
+Strings are used for storing text.
+A string variable contains a collection of characters surrounded by double quotes.
+Before declaring a variable as string the string datatype must be used
+
+ for eg - string name = "Chef";
+
+
+TASK:
+
+/*
+Write a program which does the following
+1. Create a string named fruit and assign the value "Apple" to it.
+2. Output fruit to the console.
+*/
+ 
+SOLUTION:
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  string fruit = "Apple";
+  cout << fruit ;
+  return 0;
+}
+
+
+Output: Apple
